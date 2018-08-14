f0c2b83bb53b89e540c6d18f2a7c9e2928cedca0
Randy Weyer <randy@>
2018-08-14 13:39:28 -0700

    add animation to t in upper left corner.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'glyphicons'
+import 'bootstrap';
 
 if (environment.production) {
   enableProdMode();
